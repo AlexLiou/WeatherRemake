686e2817b7fa1b3eb62a9073e600fe1a76508b22
alexliou <alexliou0@gmail.com>
2022-06-20 18:34:19 -0400

    Initial Commit

---
+++ WeatherRemake/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WeatherRemake
+//
+//  Created by Alex Liou on 6/20/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ WeatherRemake/WeatherRemakeApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherRemakeApp.swift
+//  WeatherRemake
+//
+//  Created by Alex Liou on 6/20/22.
+//
+
+import SwiftUI
+
+@main
+struct WeatherRemakeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
